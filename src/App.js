c9ff9792e9983fd39c730df72f50c97f1844d612
nic <nrhoda14@gmail.com>
2023-02-18 20:46:02 -0600

    added basic html markup and updated package.json

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
-// add code later
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+    return<Header />;
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <h3>Nicholas Rhoda</h3>
+        </div>
+    ) 
+};
